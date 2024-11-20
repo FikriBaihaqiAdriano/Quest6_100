1b9bac055b8da9986acca7fd9392791f748bebb5
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2024-11-20 17:04:18 +0700

    codingan saat di kelas

---
+++ app/src/main/java/com/example/navigationwithdata/data/RuangKelas.kt
@@ -0,0 +1,10 @@
+package com.example.navigationwithdata.data
+
+object RuangKelas {
+    val listKelas = listOf(
+        "A",
+        "B",
+        "C",
+        "D",
+    )
+}
---
+++ app/src/main/java/com/example/navigationwithdata/model/RencanaStudy.kt
@@ -0,0 +1,6 @@
+package com.example.navigationwithdata.model
+
+data class RencanaStudy(
+    val mataKuliah:String = "",
+    val kelas:String = ""
+)
